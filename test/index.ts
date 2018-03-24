ed1cd1997e3a734fb058a32079519e284e62a2b7
Pablo Rodríguez <pabloviolin8@gmail.com>
2018-03-24 14:17:33 +0100

    Fix the tests

--- test/index.ts
+++ test/index.ts
@@ -5,7 +5,7 @@ import { randomNumber } from "../src/randomNumber"
 describe("randomNumber", () => {
 	it("should generate a random number between 1 and 10", () => {
 		const test = () => {
-			const number = randomNumber(11, 20)
+			const number = randomNumber(1, 10)
 			expect(number).to.be.at.least(1)
 			expect(number).to.be.at.most(10)
 		}
